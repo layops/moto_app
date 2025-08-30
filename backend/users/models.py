ccafa3ec084de8793b8f5b83977aedf2da00012e
layops <emre.celik.290@gmail.com>
2025-08-30 16:51:54 +0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True, unique=True)
     address = models.CharField(max_length=255, blank=True, null=True)
