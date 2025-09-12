92949b8490666e11a28e300717fb9a3cffe3f14b
layops <emre.celik.290@gmail.com>
2025-09-13 02:56:20 +0300

    🗾

--- backend/notifications/migrations/0004_notification_types_update.py
+++ backend/notifications/migrations/0004_notification_types_update.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('notifications', '0003_notification_timestamp'),
+        ('notifications', '0002_alter_notification_content_type_and_more'),
     ]
 
     operations = [
