8b87c2f620e9acee5b46a601fa2107b681085038
layops <emre.celik.290@gmail.com>
2025-09-16 23:55:58 +0300

    Update settings.py

--- backend/core_api/settings.py
+++ backend/core_api/settings.py
@@ -229,7 +229,7 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/1'),
         'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            # CLIENT_CLASS parametresi kaldırıldı - Django Redis'in yeni sürümünde gerekli değil
         },
         'KEY_PREFIX': 'motoapp',
         'TIMEOUT': 300,  # 5 minutes default
