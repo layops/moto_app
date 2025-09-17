f8fb5986eba88bd23004f98aa95b6317047aa874
layops <emre.celik.290@gmail.com>
2025-09-18 02:41:49 +0300

    📠

--- backend/notifications/migrations/0005_add_notification_preferences_fields.py
+++ backend/notifications/migrations/0005_add_notification_preferences_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('notifications', '0001_initial'),
+        ('notifications', '0004_notification_types_update'),
     ]
 
     operations = [
