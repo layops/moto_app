624cc289fe3b45d92e4e4bc544604564817538e8
layops <emre.celik.290@gmail.com>
2025-09-20 02:58:28 +0300

    🔑

--- backend/add_missing_likes_comments_follows.sql
+++
@@ -1,6 +0,0 @@
--- Sadece eksik olan likes_comments ve follows sütunlarını ekle
-ALTER TABLE notifications_notificationpreferences 
-ADD COLUMN likes_comments BOOLEAN DEFAULT TRUE;
-
-ALTER TABLE notifications_notificationpreferences 
-ADD COLUMN follows BOOLEAN DEFAULT TRUE;
--- backend/check_notification_preferences_columns.sql
+++
@@ -1,5 +0,0 @@
--- NotificationPreferences tablosundaki mevcut sütunları kontrol et
-SELECT column_name, data_type, is_nullable, column_default
-FROM information_schema.columns 
-WHERE table_name = 'notifications_notificationpreferences'
-ORDER BY ordinal_position;
